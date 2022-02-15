b3a0a5b3c8e5c294cd34fb2c4d4651ad3802887a
Zhu <gangzhucoll1990@live.com>
2022-02-16 00:48:35 +0800

    first commit

---
+++ StalterWaldorfCorp.LocationService/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StatlerWaldorfCorp.LocationService.Models;
+using StatlerWaldorfCorp.LocationService.Persistence;
+using Microsoft.Extensions.Logging;
+
+namespace StatlerWaldorfCorp.LocationService
+{
+    public class Startup
+    {
+        public static string[] Args { get; set; } = new string[] { };
+        private ILogger logger;
+        private ILoggerFactory loggerFactory;
+
+        public Startup(IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(Startup.Args);
+
+            Configuration = builder.Build();
+
+            this.loggerFactory = loggerFactory;
+            //this.loggerFactory.AddConsole(LogLevel.Information);
+            //this.loggerFactory.AddDebug();
+
+            this.logger = this.loggerFactory.CreateLogger("Startup");
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
+
+            services.AddScoped<ILocationRecordRepository, InMemoryLocationRecordRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
